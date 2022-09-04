7e2c3f53ffe9763a752fbcd2230fb325d39a8acf
Pawel Walicki <pawel.walicki89@interia.pl>
2022-09-04 16:26:37 +0200

    added function to JS, little refactoring CSS, HMTL

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
-console.log("Siema to ja, a new guy ;)");
-
-let button = document.querySelector(".containter__button");
-let title = document.querySelector(".title");
-
-button.addEventListener("click", () => {
-    if (button.innerText === "Inny nagłówek") {
-        title.innerText = "ale przystojniak z tego ziomka na zdjęciu :)";
-        button.innerText = "Poprzedni nagłówek";
-    } else {
-        title.innerText = "Paweł Walicki"
-        button.innerText = "Inny nagłówek"
+{
+    const welcome = () => {
+        console.log("Siema to ja, a new guy ;)");
     }
-});
+
+    const toggleTitle = () => {
+
+        const title = document.querySelector(".js-container__title");
+
+        if (button.innerText === "Inny nagłówek") {
+            title.innerText = "ale przystojniak z tego ziomka na zdjęciu :)";
+            button.innerText = "Poprzedni nagłówek"
+        } else {
+            title.innerText = "Paweł Walicki"
+            button.innerText = "Inny nagłówek"
+        }
+    };
+
+    const init = () => {
+
+        const button = document.querySelector(".js-container__button");
+        button.addEventListener("click", toggleTitle);
+
+        welcome();
+    }
+    init();
+}
