5659e92ec68c11df645ac7fa96abf48baf2760f7
Pawel Walicki <pawel.walicki89@interia.pl>
2022-09-11 18:35:59 +0200

    bugs fixed CSS and JS

--- js/script.js
+++ js/script.js
@@ -3,8 +3,17 @@
         console.log("Siema to ja, a new guy ;)");
     }
 
-    const toggleTitle = () => {
+    const init = () => {
         const button = document.querySelector(".js-container__button");
+
+        button.addEventListener("click", () => {
+            toggleTitle(button);
+        });
+
+        welcome();
+    };
+
+    const toggleTitle = (button) => {
         const title = document.querySelector(".js-container__title");
 
         if (button.innerText === "Inny nagłówek") {
@@ -14,8 +23,7 @@
             title.innerText = "Paweł Walicki"
             button.innerText = "Inny nagłówek"
         }
-        button.addEventListener("click", toggleTitle);
-    }    
+    }
 
-    welcome();
+    init();
 }
